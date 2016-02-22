844f3a2e4ce2f60cfb94a1daeeab5ce1077c0775
Stefan O'Rear <stefanor@cox.net>
2016-02-22 14:11:40 -0800

    success/error should not be on the main object

--- index.js
+++ index.js
@@ -11,22 +11,15 @@ class RueQueue {
       throw "must provide name";
     }
 
-    console.log("creating RueQueue", params.name);
-
     this.name = params.name;
     this._queue = [];
     this.maxsize = params.maxsize;
     this.callback = params.callback;
     this.retryWait = params.retryWait || 5000;
-    this.verbose = params.verbose || false;
     this._drainRetryTimer = null;
   }
 
-  log() {
-    console.log(`RueQueue(${this.name})`, ...arguments);
-  };
-
-  push(val,doDrain) {
+  push(val, drain) {
     if (this._queue.length+1 > this.maxsize) {
 
       var maybe_regret = this._queue.pop(); // end of the array is the oldest data
@@ -42,23 +35,9 @@ class RueQueue {
 
     this._queue.unshift(val);
 
-    // if they don't specify anything assume 'drain'
-    if (typeof doDrain === 'undefined') {
+    if (drain !== false) {
       this._drain();
     }
-  }
-
-  pushNoDrain(val) {
-    this.push(val,false);
-  }
-
-  success() {
-    this._queue.pop();
-    this._drain();
-  }
-
-  error() {
-    this.resetDrainRetryTimer();
   }
 
   _drain() {
@@ -66,13 +45,17 @@ class RueQueue {
       return; // We stop trying to process messages until more are enqueued
     }
 
-    var entry = this._queue[this._queue.length-1]; // peek
-    try {
-      this.callback(entry,this);
-    } catch(e) {
-      this.log(`callback died with: ${e}`);
-      this.resetDrainRetryTimer();
-    }
+    let entry = this._queue[this._queue.length-1]; // peek
+    this.callback(entry, {
+      success: () => {
+        this._queue.pop();
+        this._drain();
+      },
+
+      error: () => {
+        this.resetDrainRetryTimer();
+      },
+    });
   }
 
   resetDrainRetryTimer() {
